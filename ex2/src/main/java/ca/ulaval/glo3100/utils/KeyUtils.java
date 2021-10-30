9cf5245f4f9aa687d8ab84800147818a621b0f9d
Fabien Roy <fabienroy28@gmail.com>
2021-10-30 11:20:36 -0400

    enhance key creation

--- ex2/src/main/java/ca/ulaval/glo3100/utils/KeyUtils.java
+++ ex2/src/main/java/ca/ulaval/glo3100/utils/KeyUtils.java
@@ -10,19 +10,21 @@ import java.security.spec.KeySpec;
 
 public class KeyUtils {
 
+    private static final String SECRET_KEY_FACTORY_PARAM = "PBKDF2WithHmacSHA1";
     private static final String ENCRYPTION = "AES";
     private static final String SALT = "SUPER_SECRET_SALT";
+    private static final int PBE_SPEC_PARAM = 100;
+    private static final int KEY_SIZE = 128;
     private static final int IV_SIZE = 16;
 
-    // TODO : Cleanup
     /**
      * @param password password as string
      * @return secret key built from password
      */
     public static SecretKey toSecretKey(String password) {
         try {
-            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-            KeySpec spec = new PBEKeySpec(password.toCharArray(), SALT.getBytes(), 65536, 256);
+            SecretKeyFactory factory = SecretKeyFactory.getInstance(SECRET_KEY_FACTORY_PARAM);
+            KeySpec spec = new PBEKeySpec(password.toCharArray(), SALT.getBytes(), PBE_SPEC_PARAM, KEY_SIZE);
             SecretKey tmp = factory.generateSecret(spec);
             return new SecretKeySpec(tmp.getEncoded(), ENCRYPTION);
         } catch (Exception e) {
