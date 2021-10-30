8d3d084d6a804433cb287ef5a0248711d156515e
Fabien Roy <fabienroy28@gmail.com>
2021-10-30 12:56:48 -0400

    Use AES CTR

--- ex1/src/main/java/ca/ulaval/glo3100/utils/CipherUtils.java
+++ ex1/src/main/java/ca/ulaval/glo3100/utils/CipherUtils.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class CipherUtils {
 
-    private static final String TRANSFORMATION = "AES/CBC/PKCS5Padding";
+    private static final String TRANSFORMATION = "AES/CTR/NoPadding";
 
     /**
      * @param originalFilesAsBytes Original files as bytes
